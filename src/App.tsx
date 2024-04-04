e5a0be9cb416ffa9e0486c37dbb1dfefebcff5a3
Vaibhav Matere <vpm00718897@gmail.com>
2024-04-04 15:38:31 +0530

    feat : added default tasks & set types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import './App.css';
+import { Task } from '././utilities/constant.ts';
 
 function App() {
+    const tasks: Task[] = [
+        {
+            id: 1,
+            title: 'Create React App',
+            isCompleted: false,
+            priority: 'p2',
+        },
+        {
+            id: 2,
+            title: 'Solve Assignment',
+            isCompleted: true,
+            priority: 'p1',
+        },
+        {
+            id: 3,
+            title: 'Write Unit Tests',
+            isCompleted: false,
+            priority: 'p3',
+        },
+    ];
     return (
         <>
             <h1>Todo App</h1>
+            {tasks.map((task) => (
+                <li key={task.id}>{task.title}</li>
+            ))}
         </>
     );
 }
---
+++ src/utilities/constant.ts
@@ -0,0 +1,8 @@
+export type Priority = 'p1' | 'p2' | 'p3';
+
+export type Task = {
+    id: number;
+    title: string;
+    isCompleted: boolean;
+    priority?: Priority; // optional
+};
