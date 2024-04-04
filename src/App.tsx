a67eaaebab1b31296b08f07fbf99f1165b0826a8
Vaibhav Matere <vpm00718897@gmail.com>
2024-04-04 15:49:28 +0530

    refactor : file structure & layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,12 @@
 import './App.css';
-import { Task } from '././utilities/constant.ts';
+import { tasks } from './utilities/constants';
 
 function App() {
-    const tasks: Task[] = [
-        {
-            id: 1,
-            title: 'Create React App',
-            isCompleted: false,
-            priority: 'p2',
-        },
-        {
-            id: 2,
-            title: 'Solve Assignment',
-            isCompleted: true,
-            priority: 'p1',
-        },
-        {
-            id: 3,
-            title: 'Write Unit Tests',
-            isCompleted: false,
-            priority: 'p3',
-        },
-    ];
     return (
         <>
             <h1>Todo App</h1>
+            <label htmlFor="task-input">Add task: </label>
+            <button>Add task</button>
             {tasks.map((task) => (
                 <li key={task.id}>{task.title}</li>
             ))}
---
+++ src/utilities/constants.ts
@@ -0,0 +1,22 @@
+import { Task } from './types.ts';
+
+export const tasks: Task[] = [
+    {
+        id: 1,
+        title: 'Create React App',
+        isCompleted: false,
+        priority: 'p2',
+    },
+    {
+        id: 2,
+        title: 'Solve Assignment',
+        isCompleted: true,
+        priority: 'p1',
+    },
+    {
+        id: 3,
+        title: 'Write Unit Tests',
+        isCompleted: false,
+        priority: 'p3',
+    },
+];
